3f77193447bf3f3483cbb956dddaa5fd9509b43c
VanjaRo <vanjaro43@gmail.com>
2020-10-07 13:10:39 +0300

    Added a bracket in log output

--- Shell_lab/log_.sh
+++ Shell_lab/log_.sh
@@ -6,8 +6,8 @@ out=/var/log/anaconda/X.log
 
 if [[ (-f $out) && (-r $out) ]]
 then
-    sed -n 's/] (WW)/Warning/p' $out > tmp.txt
-    sed -n 's/] (II)/Information/p' $out >> tmp.txt
+    sed -n 's/] (WW)/] Warning/p' $out > tmp.txt
+    sed -n 's/] (II)/] Information/p' $out >> tmp.txt
 
     GREP_COLOR="1;33" grep --color="always" "Warning" tmp.txt
     GREP_COLOR="1;34" grep --color="always" "Information" tmp.txt
